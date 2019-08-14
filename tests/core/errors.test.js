68463a5075873d047001a172497b50dfd0779258
Charles Paulet <charles.paulet@epitech.eu>
2019-08-14 14:40:15 +0200

    fix: avoid experimental syntax on err throwing

--- tests/core/errors.test.js
+++ tests/core/errors.test.js
@@ -7,25 +7,33 @@ import {
 
 describe("Errors", () => {
   test("TemplatesError", () => {
-    const t = () => throw new TemplatesError();
+    const t = () => {
+      throw new TemplatesError();
+    };
     expect(t).toThrow(TemplatesError);
     expect(t).toThrow(Error);
   });
 
   test("InterfaceError", () => {
-    const t = () => throw new InterfaceError();
+    const t = () => {
+      throw new InterfaceError();
+    };
     expect(t).toThrow(InterfaceError);
     expect(t).toThrow(Error);
   });
 
   test("FormatterError", () => {
-    const t = () => throw new FormatterError();
+    const t = () => {
+      throw new FormatterError();
+    };
     expect(t).toThrow(FormatterError);
     expect(t).toThrow(Error);
   });
 
   test("MutationError", () => {
-    const t = () => throw new MutationError();
+    const t = () => {
+      throw new MutationError();
+    };
     expect(t).toThrow(MutationError);
     expect(t).toThrow(Error);
   });
